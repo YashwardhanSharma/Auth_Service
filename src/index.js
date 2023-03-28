4ea35471f4d1cd33c576eb42a38902b9e0ff94a2
YashwardhanSharma <yash96904@gmail.com>
2023-03-28 10:23:51 -0700

    Added jwt create and verify logic

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ const bodyParser=require('body-parser');
 const {PORT} = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
 
-// const {User} = require('./models/index');
-// const bcrypt =  require('bcrypt');
-const UserRepository = require('./repository/user-repository');
+//const UserService = require('./services/user-service');
+
 
 
 const app = express();
@@ -19,13 +18,13 @@ app.use('/api',apiRoutes);
 
 app.listen(PORT,async ()=>{
     console.log(`Server Started on Port : ${PORT}`);
-    // const repo = new UserRepository();
-    // const response = await repo.getById(1);
-    // console.log(response);
-    // const incomingp='123456';
-    // const user = await User.findByPk(7);
-    // const response = bcrypt.compareSync(incomingp,user.password);
-    // console.log(response);
+   
+    // const service = new UserService();
+    // const newToken =  service.createToken({email:'yash@gmail.com',id:1});
+    // console.log("New token is",newToken);
+    //  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Inlhc2hAZ21haWwuY29tIiwiaWQiOjEsImlhdCI6MTY4MDAyMzg0MywiZXhwIjoxNjgwMDIzODczfQ.j9r_yQ_YcS0Fs4NEziKjgYy0htQHZWaduvcmbyhNO3A';
+    //  const response = service.verifyToken(token);
+    //  console.log(response);
 });
 }
 
